612591338e84ac6afca00a941f7f735816498fe4
Ian <ihmorris@ualberta.ca>
2023-05-03 10:00:02 -0600

    Script in header runs on button press event

--- helloworld.js
+++ helloworld.js
@@ -1,13 +1,19 @@
 <html>
-    <body>
+    <head>
         <script language="javascript" type="text/javascript">
         <!--
-            document.write("Hello World!")
+            function sayHello() {
+                alert("Hello World!")   
+            }
         //-->
         </script>
 
         <noscript>
             Sorry... JavaScript is needed to continue.
         </noscript>
+    </head>
+
+    <body>
+        <input type="button" onclick="sayHello()" value="Say Hello"/>
     </body>
 </html>
