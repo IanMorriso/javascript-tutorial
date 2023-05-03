223ca4c61587acb1cc9750af2b168b95c8e0a3aa
Ian <ihmorris@ualberta.ca>
2023-05-03 09:44:40 -0600

    Hello World!

---
+++ helloworld.js
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <script language="javascript" type="text/javascript">
+        <!--
+            document.write("Hello World!")
+        //-->
+        </script>
+
+        <noscript>
+            Sorry... JavaScript is needed to continue.
+        </noscript>
+    </body>
+</html>
